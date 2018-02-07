195bb9baef4ae54803bf26b6974fd4b45a954a50
Vinícius Garcia <vingarciasc@gmail.com>
2018-02-06 23:20:22 -0200

    v0.1.0
    initial commit

---
+++ Assets/example.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RawMouseDriver;
+using RawInputSharp;
+ 
+public class example : MonoBehaviour
+{
+    private RawMouseDriver.RawMouseDriver mousedriver;
+    private RawMouse mouse1;
+    private RawMouse mouse2;
+ 
+    private float moveY = 0.0f;
+    private float moveX = 0.0f;
+ 
+    void Start ()
+    {
+        mousedriver = new RawMouseDriver.RawMouseDriver ();
+    }
+    void Update()
+    {
+		print("moveX: " + moveX + ", moveY: " + moveY);
+        mousedriver.GetMouse (0, ref mouse1);
+        mousedriver.GetMouse (1, ref mouse2);
+     
+        moveY += mouse1.YDelta;
+        transform.Translate(Vector3.forward * moveY);
+     
+        moveX += mouse2.XDelta;
+        transform.Translate(Vector3.forward * moveX);
+    }
+    void OnApplicationQuit()
+    {
+        mousedriver.Dispose ();
+    }
+}
+ 
