4d46e640823437b74231d9c80d9a182ae618b70f
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-02-12 17:01:16 -0200

    Estrutura básica do menu

---
+++ Assets/Resources/Scripts/System/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuController : MonoBehaviour {
+
+	[SerializeField] private List<Transform> cameraPositions;
+
+	public void ChangeCameraPosition(int i){
+		StartCoroutine( InterpolateToTransform(cameraPositions[i]) );
+	}
+	private IEnumerator InterpolateToTransform(Transform t){
+		//Transita para a posicao t.position e a rotacao t.rotation e em escala t.scale
+		DOTween.Init();
+		transform.DOMove(t.localPosition,1f);
+		transform.DOPlay();
+		yield return new WaitForSeconds(1f);
+		print("pppppp");
+	}
+}
+public enum CameraState{
+	MAIN_MENU,SELECTION_MENU
+}
