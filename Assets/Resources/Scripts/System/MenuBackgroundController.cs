4a84036deb48ae136a1cf6c2dadba7ef03e1c522
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-02-18 19:59:45 -0300

    ...

--- Assets/Resources/Scripts/System/MenuBackgroundController.cs
+++ Assets/Resources/Scripts/System/MenuBackgroundController.cs
@@ -14,7 +14,7 @@ public class MenuBackgroundController : MonoBehaviour {
 	}
 	private IEnumerator BallShooter(){
 		while(true){
-			Vector3 dir = 100f*(Vector3.up*Random.Range(-1f,1f) + Vector3.right*Random.Range(-1f,1f));
+			Vector3 dir = 3000f*(Vector3.up*Random.Range(-1f,1f) + Vector3.right*Random.Range(-1f,1f));
 			balls[ Random.Range(0,balls.Count) ].GetComponent<Rigidbody>().AddForce(dir);
 			//play audioclip
 			yield return new WaitForSeconds(timeBetweenShoots);
