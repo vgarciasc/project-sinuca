2c9d8de788f648d95cc0844f3ad0e96465aeec03
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-02-18 16:02:47 -0300

    Tela de tutorial e fonte

--- Assets/Resources/Scripts/System/MenuController.cs
+++ Assets/Resources/Scripts/System/MenuController.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class MenuController : MonoBehaviour {
 
 	[SerializeField] private List<Transform> cameraPositions;
+	[SerializeField] private List<GameObject> tutorialPanels;
 
 	public void ChangeCameraPosition(int i){
 		StartCoroutine( InterpolateToTransform(cameraPositions[i]) );
@@ -16,6 +17,20 @@ public class MenuController : MonoBehaviour {
 		transform.DOMove(t.localPosition,1f);
 		transform.DOPlay();
 		yield return new WaitForSeconds(1f);
+	}
+	public void TutorialPanel(){
+		StartCoroutine( TutorialPanelAnimation() );
+	}
+	private IEnumerator TutorialPanelAnimation(){
+		float unit = tutorialPanels[0].transform.localScale.y;
+		foreach(GameObject go in tutorialPanels){
+			go.transform.localScale = new Vector3(0,unit,unit);
+		}
+		yield return new WaitForSeconds(0.3f);
+		foreach(GameObject go in tutorialPanels){
+			go.transform.DOScaleX(unit,0.3f);
+			yield return new WaitForSeconds(0.3f);
+		}
 	}
 }
 public enum CameraState{
